86b17e859183f621b234cf149f1256483fa92637
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 10:06:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Your code here
 
 function mapToNegativize(srcArr) {
+  
   let newArr = [];
+  
   for (let i = 0; i < srcArr.length; i++) {
     newArr.push(srcArr[i] * -1);
   }
@@ -11,10 +13,29 @@ function mapToNegativize(srcArr) {
 
 
 function mapToDouble(srcArr) {
+  
   let newArr = [];
+  
   for (let i = 0; i < srcArr.length; i++) {
     newArr.push(srcArr[i] * 2);
   }
   
   return newArr;
 }
+
+function mapToSquare(srcArr) {
+  let newArr = [];
+  
+  for (let i = 0; i < srcArr.length; i++) {
+    newArr.push(srcArr[i] ** 2);
+  }
+  
+  return newArr;
+}
+
+
+
+
+
+
+
