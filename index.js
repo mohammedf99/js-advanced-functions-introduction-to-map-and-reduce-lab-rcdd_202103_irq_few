0e74ba11ea6f31eb53f28ab5625ea5015842edec
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 10:00:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Your code here
 
 function mapToNegativize(srcArr) {
+  let newArr = [];
+  for (let i = 0; i < srcArr.length; i++) {
+    newArr.push(srcArr[i] * 1);
+  }
   
+  return newArr;
 }
